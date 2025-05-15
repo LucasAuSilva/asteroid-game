74af6db86744e54ccae1a5a37347f8ccc4181d5e
LucasAuSilva <silvaaugustolucas@gmail.com>
2025-05-15 20:18:53 -0300

    drawing the player spacesip in the screen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     print("Starting Asteroids!")
@@ -7,13 +8,18 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
     print("")
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill(color="black")
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000 # miliseconds to seconds
 
 if __name__ == "__main__":
     main()
